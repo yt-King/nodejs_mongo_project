97de07d75dc94dbeb8426d45fdf9d06d2ab2d35d
应涛 <yt1577396227@163.com>
2021-12-12 22:58:40 +0800

    后台管理页面设计

--- web/src/main.js
+++ web/src/main.js
@@ -2,14 +2,24 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import router from './router'
+import store from './store'
 import axios from "axios";
 import http from './http'
 
+Vue.config.productionTip = false
 Vue.prototype.$http = http
 
 Vue.use(ElementUI);
 
 new Vue({
-  el: '#app',
+  router,
+  store,
   render: h => h(App)
-});
+}).$mount('#app')
+
+
+
+
+
+
