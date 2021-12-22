a5069bba9cf127564e3e796e9ae26df743d43ff4
应涛 <yt1577396227@163.com>
2021-12-22 23:11:43 +0800

    修改数据库连接

--- nodeDocker/node/db/index.js
+++ nodeDocker/node/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 //抛出用于连接数据库的方法
 module.exports = ()=>{
-    mongoose.connect('mongodb://admin:zufe777@150.158.28.238:27017/190110910532?authSource=admin',{useNewUrlParser:true,useUnifiedTopology:true })
+    mongoose.connect('你的数据库地址。。。。。。。。。。',{useNewUrlParser:true,useUnifiedTopology:true })
     .then(()=>{
         console.log('数据库连接成功')
     }).catch(err=>{
